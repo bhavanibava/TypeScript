8ea333cae46f70c4309d8abc948f042bb0c72b2e
bhavanibava <39649592+bhavanibava@users.noreply.github.com>
2018-08-28 17:09:06 +0530

    addition

--- type/numberType.ts
+++ type/numberType.ts
@@ -1,10 +1,9 @@
 class addition {
     addfun(a:number,b:number){
-        return a+b;
+        return `sum of ${a} and ${b} is ${a+b}`;
     } 
 }
 let a:number = 20;
 let b:number = 10;
 let add_obj = new addition();
-let result = add_obj.addfun(a,b);
-console.log("add a and b value :"+result);
+console.log(add_obj.addfun(a,b));
